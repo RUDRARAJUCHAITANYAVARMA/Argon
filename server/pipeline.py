e824b0d7238832886397a4405c20303726abdfa7
Chaitu5210 <chaitanyarudraraju5210@gmail.com>
2026-06-24 21:57:31 +0530

    refactor: update logging configuration, adjust data retrieval window to 2 days, and trigger pipeline execution

--- server/pipeline.py
+++ server/pipeline.py
@@ -1,20 +1,29 @@
-from IPython.core import logger
+import logging
 from datetime import timedelta
 from datetime import datetime
-from server.news.news_api import get_top_headlines
+from news.news_api import get_top_headlines
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
 
 
 def pipeline():
 
     logger.info("Starting the pipeline")
 
-    date = datetime.now() - timedelta(days=1)
+    date = datetime.now() - timedelta(days=2)
 
     try:
+
+        # Stage 1 - Retrieving the news
         logger.info(f"Fetching news for date : {date}")
         news = get_top_headlines(date)
-        logger.info(f"fetched {len(news)} news")
+        logger.info(f"Successfully fetched {len(news)} news")
 
     except Exception as e:
         logger.exception("Pipeline Failed with Exception : {e}")
         raise
+
+
+pipeline()
