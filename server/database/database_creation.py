255dfc505b0fbbbc08605c6cac45aef3a8416607
Chaitu5210 <chaitanyarudraraju5210@gmail.com>
2026-06-24 22:39:21 +0530

    feat: implement database initialization and integrate into pipeline execution

---
+++ server/database/database_creation.py
@@ -0,0 +1,27 @@
+import sqlite3
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+def initialize_article_db(db_name="news.db"):
+
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS articles (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            description TEXT,
+            content TEXT NOT NULL
+        )
+    """
+    )
+
+    connection.commit()
+    connection.close()
+    logger.info(f"Successfully initiallized database '{db_name}'")
--- server/pipeline.py
+++ server/pipeline.py
@@ -2,6 +2,7 @@ import logging
 from datetime import timedelta
 from datetime import datetime
 from news.news_api import get_top_headlines
+from database.database_creation import initialize_article_db
 
 logging.basicConfig(level=logging.INFO)
 
@@ -20,6 +21,11 @@ def pipeline():
         logger.info(f"Fetching news for date : {date}")
         news = get_top_headlines(date)
         logger.info(f"Successfully fetched {len(news)} news")
+
+        # Stage 2 - Initializing the database
+        logger.info("Initializing the database")
+        initialize_article_db()
+        logger.info("Successfully initialized the database")
 
     except Exception as e:
         logger.exception("Pipeline Failed with Exception : {e}")
